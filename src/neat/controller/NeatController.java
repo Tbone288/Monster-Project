043eb2071762e7bb8d2180df928588b9174588ca
Tbone288 <todo3858@740s-pl-1176.local>
2016-09-14 12:52:57 -0600

    Created monster

--- src/neat/controller/NeatController.java
+++ src/neat/controller/NeatController.java
@@ -1,9 +1,29 @@
 package neat.controller;
 
+import neat.model.HowNeat;
+
 public class NeatController 
 {
+	private HowNeat firstMonster;
+	
+	public NeatController()
+	{
+		firstMonster = new HowNeat("Steve", 2.33, 1, 1, 2, false);
+	}
+	
 	public void start()
 	{
-		
+		System.out.println("We made monsters today!");
+		System.out.println("Here is mine" + firstMonster);
 	}
+	
+	/*
+	 * monster name is Steve
+	 * has 2.33 hair
+	 * has 1 arm
+	 * has 1 nose
+	 * has 2 eye
+	 * no belly button
+	 */
 }
+
