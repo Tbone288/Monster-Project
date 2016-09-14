afc78c98ac9f97249349182c914b37562ef65509
Tbone288 <todo3858@740s-pl-1176.local>
2016-09-14 12:26:04 -0600

    added constuctor

--- src/neat/model/HowNeat.java
+++ src/neat/model/HowNeat.java
@@ -1,5 +1,31 @@
 package neat.model;
 
-public class HowNeat {
-
+public class HowNeat 
+{
+	private String name;
+	private double hairCount;
+	private int eyeCount;
+	private int armCount;
+	private int noseCount;
+	private boolean hasBellyButton;
+	
+	public HowNeat()
+	{
+		this.name = "no name";
+		this.hairCount = -2345.23;
+		this.eyeCount = -2345;
+		this.armCount = -324;
+		this.noseCount = -2355;
+		this.hasBellyButton = false;
+	}
+	
+	public HowNeat(String name, double hairCount, int eyeCount, int armCount, int noseCount, boolean hasBellyButton)
+	{
+		this.name = name;
+		this.hairCount = hairCount;
+		this.eyeCount = eyeCount;
+		this.armCount = armCount;
+		this.noseCount = noseCount;
+		this.hasBellyButton = false;
+	}
 }
